e4c1e1cd97b0e8209ab023d9845b7642d7e5a6d6
Folasade Ekulona <ekulonaf@gmail.com>
2023-03-11 16:38:42 -0500

    adds completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis", 
+    "Garfield",
+];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+};
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+};
+function destructivelyRemoveLastCat(name) {
+    cats.pop()
+};
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+};
+function appendCat(name) {
+    return [...cats, name]
+};
+function prependCat(name) {
+    return [name, ...cats]
+};
+function removeLastCat(name) {
+    const newCats = cats.slice()
+    return newCats.slice(0, newCats.length -1)
+};
+function removeFirstCat(name) {
+    const newerCats = cats.slice()
+    return newerCats.slice(1)
+};
